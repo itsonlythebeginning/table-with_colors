2b2b48887363c22e7843ab91a0006792a17ac201
itsonlythebeginning <107440223+itsonlythebeginning@users.noreply.github.com>
2022-06-22 11:09:10 +0300

    colors

---
+++ app.js
@@ -0,0 +1,62 @@
+
+
+let a = document.getElementById("changeColor")
+
+let m  = document.getElementById("app")
+
+
+
+
+let ddd = document.getElementById("one")
+let eee = document.getElementById("two")
+let fff = document.getElementById("three")
+let ggg = document.getElementById("four")
+let hhh = document.getElementById("five")
+
+
+let f = document.getElementById("odin")
+let g = document.getElementById("dva")
+let h = document.getElementById("tree")
+let k = document.getElementById("chetiry")
+let l = document.getElementById("pyat")
+
+
+let b = document.getElementById("backTop")
+
+
+f.style.backgroundColor = "green"
+
+
+function changing () {
+
+    b.style.transform = `rotateY(${m.value}deg)`
+
+}
+function change1 () {
+
+    f.style.backgroundColor = `${ddd.value}`
+
+}
+function change2 () {
+
+    g.style.backgroundColor = `${eee.value}`
+
+}
+
+function change3 () {
+
+    h.style.backgroundColor = `${fff.value}`
+
+}
+
+function change4 () {
+
+    k.style.backgroundColor = `${ggg.value}`
+
+}
+
+function change5 () {
+
+    l.style.backgroundColor = `${hhh.value}`
+
+}
